4604b749477703de6bf042d78dce30932cd9dcc1
blazebomb <ashishkumar850601@gmail.com>
2025-08-05 20:03:27 +0530

    final -3 alpha

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "app/generated/**/*",
+      "**/generated/**/*",
+      "**/.next/**/*",
+      "**/node_modules/**/*",
+      "**/dist/**/*",
+      "**/build/**/*"
+    ]
+  }
 ];
 
 export default eslintConfig;
